4741bc58ed8012768783b5258577dc83da668171
Damian Kaczmarczyk <damiankaczmarczyk0@gmail.com>
2021-08-26 20:30:16 +0200

    fill empty space

--- src/com/dog/Main.java
+++ src/com/dog/Main.java
@@ -1,6 +1,7 @@
 package com.dog;
 
 import javax.swing.*;
+import java.awt.*;
 import java.io.IOException;
 
 public class Main {
@@ -8,19 +9,25 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         JFrame frame = new JFrame("Shibecore");
-        frame.setSize(1000,600);
+        frame.setLayout(null);
+        frame.setBounds(0,0,1000,600);
         JLabel label = new JLabel();
         NextDoge nextDoge = new NextDoge(label);
 
         PreviousDoge previousDogeButton = new PreviousDoge(label);
+        JLabel filler = new JLabel(new ImageIcon(
+            new ImageIcon("resources/shiba.jpg")
+                .getImage()
+                .getScaledInstance(200,400, Image.SCALE_SMOOTH)));
+        filler.setBounds(0,200,200,400);
+        frame.add(filler);
 
-        frame.setVisible(true);
-        frame.setLayout(null);
         frame.setResizable(false);
         frame.add(nextDoge.getButton());
         frame.add(previousDogeButton.getButton());
         frame.add(label);
         label.setBounds(200,0,800,600);
         nextDoge.getDog();
+        frame.setVisible(true);
     }
 }
